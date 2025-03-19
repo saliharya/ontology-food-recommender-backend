3476c293d11b9771204024466ddc18fb8ce8970a
Salih Arya Gumilang <salih.arya@getgoing.co.id>
2025-03-19 07:19:02 +0700

    init

---
+++ src/main/java/com/dea/foodrecommenderbackend/FoodRecommenderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dea.foodrecommenderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecommenderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRecommenderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dea/foodrecommenderbackend/FoodRecommenderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dea.foodrecommenderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecommenderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
